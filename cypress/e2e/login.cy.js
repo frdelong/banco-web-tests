068b9dbe646b51abc72df76ba45571be80ad64f4
frdelong <frdelong@gmail.com>
2025-08-25 22:15:40 -0300

    refactor: update login tests to use environment variable for URL

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,14 @@
-describe("Login", () => {
+describe('Login', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:4000")
+    cy.visit(Cypress.env('URL'))
   })
-  it("Login com dados válidos deve permitir entrada no sistema", () => {
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Arrange
     // cy.visit("http://localhost:4000")
 
     // Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get("#username").click().type(credenciais.valida.usuario)
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
       cy.get('#senha').click().type(credenciais.valida.senha)
     })
     cy.screenshot('apos-preencher-dados-validos')
@@ -20,13 +20,13 @@ describe("Login", () => {
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
-  it("Login com dados inválidos deve apresentar mensagem de erro", () => {
+  it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     // Arrange
     // cy.visit("http://localhost:4000")
 
     // Act
-    cy.fixture('credenciais').then(credenciais =>{
-      cy.get("#username").click().type(credenciais.invalida.usuario)
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
       cy.get('#senha').click().type(credenciais.invalida.senha)
     })
     // cy.get('#login-section > .btn').click()
